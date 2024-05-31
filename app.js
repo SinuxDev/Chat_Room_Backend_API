f031296560ebebfff94ed3c1d0700b524e65df28
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-01 00:07:49 +0630

    Implement SocketIO

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const express = require("express");
+const socketIO = require("socket.io");
+const cors = require("cors");
+
+const { formatMSG } = require("./utils/formatMSG");
+
 const app = express();
 
-app.listen(3000, () => {
+app.use(cors());
+
+const server = app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+const SocketIO = socketIO(server, { cors: { origin: "*" } });
+
+// Run when client-server connection is established
+SocketIO.on("connection", (socket) => {
+  console.log("Client connected");
+
+  const BOT_NAME = "Bot Su";
+  const BOT_MSG = "Welcome to SuChat.io";
+  socket.emit("message", formatMSG(BOT_NAME, BOT_MSG));
+
+  // Listen for join room / send join room message except for the user itself
+  socket.broadcast.emit(
+    "message",
+    formatMSG(BOT_NAME, "Anonymous has joined the chat")
+  );
+
+  // User Leave the chat
+  socket.on("disconnect", () => {
+    SocketIO.emit(
+      "message",
+      formatMSG(BOT_NAME, "Anonymous has left the chat")
+    );
+  });
 });
